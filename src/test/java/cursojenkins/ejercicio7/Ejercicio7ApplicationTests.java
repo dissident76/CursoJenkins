f1da8b09633d6c30c6b39878039c2dcb8190aef2
dissident76 <dissident76@gmail.com>
2023-04-17 21:09:43 +0200

    primer commit 7ejercicio 7

---
+++ src/test/java/cursojenkins/ejercicio7/Ejercicio7ApplicationTests.java
@@ -0,0 +1,13 @@
+package cursojenkins.ejercicio7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
